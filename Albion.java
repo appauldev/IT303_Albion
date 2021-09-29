40fa1aedc28a1ced7d05569d3aabe1c9752672ca
PL S <23691370+appauldev@users.noreply.github.com>
2021-09-29 15:38:58 +0800

    Update Albion.java

--- Albion.java
+++ Albion.java
@@ -1,6 +1,8 @@
 public class Albion {
 
 	public static void main(String[] args) {
+		// initialization and declaration
+		// we used int for simplicity
 		int J = 10,
 			O = 20,
 			H = 30,
@@ -14,7 +16,7 @@ public class Albion {
 		int quotient_1 = O / J; // 20 / 10 = 2
 		int quotient_2 = N / J; // 40 / 10 = 4
 		int quotient_3 = N / H; // 40 / 30 = 1
-		// explicitly typecasting or "lantarang pagcoconvert"
+		// explicit typecasting or "lantarang pagcoconvert" para makita yung decimal
 		double quotient_4 = (double) N / H; // 40 / 30 = 1.3333
 		System.out.println();
 		System.out.println("Quotient 1: " + quotient_1);
@@ -25,9 +27,11 @@ public class Albion {
 		// modulus - the remainder kapag nagdivide
 		int mod_1 = N % H; // 40 % 30 = 10
 		int mod_2 = H % J; // 30 % 10 = 0
+		int mod_3 = H % N; // 30 % 40 = 30
 		System.out.println();
 		System.out.println("Modulo 1: " + mod_1);
 		System.out.println("Modulo 2: " + mod_2);
+		System.out.println("Modulo 3: " + mod_3);
 		
 		// decrement
 		System.out.println();
@@ -36,7 +40,6 @@ public class Albion {
 		// post is after, so gamit muna bago mag-minus
 		System.out.println("Decrement 2 (post): " + N--); // use N; N = N - 1
 		System.out.println("Value of N after post-decrement: " + N);
-
 	}
 
 }
